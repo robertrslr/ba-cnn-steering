3d82396b5499eb3fd95d9cc8ba9fa84212c1e469
robertrslr <robert.roesler@haw-hamburg.de>
2018-11-22 17:09:47 +0100

    added server/client in python 3

---
+++ Code/ipc/serverp3.py
@@ -0,0 +1,26 @@
+ -*- coding: utf-8 -*-
+import socket
+import os
+
+if os.path.exists("/tmp/python_unix_sockets_example"):
+    os.remove("/tmp/python_unix_sockets_example")
+
+print("Opening socket...")
+server = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+server.bind("/tmp/python_unix_sockets_example")
+
+print("Listening...")
+while True:
+    datagram = server.recv(1024)
+    if not datagram:
+        break
+    else:
+        print("-" * 20)
+        print(datagram.decode('utf-8'))
+        if "DONE" == datagram.decode('utf-8'):
+            break
+print("-" * 20)
+print("Shutting down...")
+server.close()
+os.remove("/tmp/python_unix_sockets_example")
+print("Done")
